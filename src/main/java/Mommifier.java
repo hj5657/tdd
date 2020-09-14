8a85f721f3e2666ee5a7e6e6dbd4c97bb03a0082
hejie <565703319@qq.com>
2020-09-14 12:07:50 +0800

    implement should return string when the number of vowels not more than 30 percent of the string length

---
+++ src/main/java/Mommifier.java
@@ -0,0 +1,8 @@
+/**
+ * Create by 木水 on 2020/9/14.
+ */
+public class Mommifier {
+    public String convert(String hhhhh) {
+        return hhhhh;
+    }
+}
--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -1,2 +1,12 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
 public class MommifierTest {
+    @Test
+    void should_return_string_when_the_number_of_vowels_not_more_than_30_percent_of_the_string_length() {
+        Mommifier mommifier = new Mommifier();
+        String result = mommifier.convert("hhhhh");
+        Assertions.assertEquals("hhhhh",result);
+    }
 }
