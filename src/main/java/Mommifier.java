357054d06c8352512f6318c288d2fa004b625af5
hejie <565703319@qq.com>
2020-09-14 13:19:18 +0800

    implement should insert mommy when the number of vowels more than 30 percent of the string length

--- src/main/java/Mommifier.java
+++ src/main/java/Mommifier.java
@@ -3,6 +3,35 @@
  */
 public class Mommifier {
     public String convert(String string) {
+        String result="";
+        if (isMore(string)){
+            for (int i=0;i<string.length()-1;i++){
+                char t1=string.charAt(i);
+                char t2=string.charAt(i+1);
+                result+=t1;
+                if (t1=='a'||t1=='e'||t1=='i'||t1=='o'||t1=='u'){
+                    if (t2=='a'||t2=='e'||t2=='i'||t2=='o'||t2=='u'){
+                        result+="mommy";
+                    }
+                }
+            }
+            result+=string.charAt(string.length()-1);
+            return result;
+        }
         return string;
+    }
+
+    private boolean isMore(String string) {
+        double length=0;
+        for (int i=0;i<string.length();i++){
+            char temp=string.charAt(i);
+            if (temp=='a'||temp=='e'||temp=='i'||temp=='o'||temp=='u'){
+                length++;
+            }
+        }
+        if (length/string.length()>0.3){
+            return true;
+        }
+        return false;
     }
 }
--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -9,5 +9,10 @@ public class MommifierTest {
         String result = mommifier.convert("hhhhh");
         Assertions.assertEquals("hhhhh",result);
     }
-
+    @Test
+    void should_insert_mommy_for_each_continus_set_vowels_when_the_number_of_vowels_more_than_30_percent_of_the_string_length() {
+        Mommifier mommifier = new Mommifier();
+        String result = mommifier.convert("aebcde");
+        Assertions.assertEquals("amommyebcde",result);
+    }
 }
